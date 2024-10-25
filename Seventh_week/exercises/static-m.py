a8e8af4d53fcf9f20994f7c2df1debea46b4c4f8
Rhoda <rhodachela@gmail.com>
2024-10-25 22:28:37 +0300

    Using class methods

--- Seventh_week/exercises/static-m.py
+++ Seventh_week/exercises/static-m.py
@@ -36,7 +36,7 @@ print(Calculator.multiply(900, 3, 4))
 
 #Practice exercise 3
 class Person:
-    def __init__(self, name, age):
+    def __init__(self, name, age):         
         self.name = name
         self.age = age
     @classmethod
@@ -46,3 +46,26 @@ class Person:
         return f"Child: {self.name}, Age: {self.age}"
 child = Person.create_child("Tashy")
 print(child)
+
+class School:
+    total_students = 0
+    def __init__(self, stream, game, subjects):
+        self.stream = stream
+        self.game = game
+        self.subjects = subjects
+        School.total_students +=1
+    def __str__(self):
+        return f"The stream {self.stream} participates in {self.game}. It has a total of {School.total_students} students taking {self.subjects} "
+    def play(self):
+        return f"All students in the stream should play games"
+    @classmethod
+    def participate(cls):
+        return f"The students who can participate are {cls.total_students}"
+    def run(self):
+        return f"The students who can run are mostly Kalenjins."
+
+school1 = School("Violet", "Volleyball", "French")
+school2 = School("Green", "Basketball", "German")
+school3 = School("Purple", "Football", "Maths")
+print(school1.participate())
+print(school3.run())
