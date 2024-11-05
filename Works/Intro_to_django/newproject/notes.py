d79c7de88d57f8ff339ddafa50f0e0753778c419
Rhoda <rhodachela@gmail.com>
2024-11-05 14:49:12 +0300

    Intro to django

---
+++ Works/Intro_to_django/newproject/notes.py
@@ -0,0 +1,9 @@
+#DJANGO PROJECT
+#Settings / configuration
+#app
+#manage.py- runserver, createapps
+
+#APP
+#Projects specific task/feature in a django project
+
+#paths/urls(uniform resource identifier)
