72e8ec0922df91a163cc5e31568f62132135ea8b
Rhoda <rhodachela@gmail.com>
2024-11-08 17:43:27 +0300

    Configuring url patterns

--- Works/Advanced-model-relationships/youtube/youtube/urls.py
+++ Works/Advanced-model-relationships/youtube/youtube/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("users.urls"))
+    path('', include("users.urls")),
+    path('hello/', views.hellow_view, name = "hello"),
+
 ]
