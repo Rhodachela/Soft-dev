d663b81daee4efb1fda052f340680b685f67dc17
Rhoda <rhodachela@gmail.com>
2024-11-11 23:10:41 +0300

    Configurings

--- Works/Advanced-model-relationships/youtube/users/views.py
+++ Works/Advanced-model-relationships/youtube/users/views.py
@@ -1,21 +1,24 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+# from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+# # Create your views here.
+# # @login_required
+# # def profile_view(request):
+# #     return render(request, "profile.html")
 
-
-def my_home(request):
-    return render(request, "base.html")
-def blog(request):
-    return render(request, "blog.html")
-def contact(request):
-    return render(request, "contact.html")
-def about(request):
-    return render(request, "about.html")
-def home(request):
-    user_data = {
-        "name": "Desmond Rono",
-        "age": 26,
-        "bio": " Engineer!",
-        "hobbies": ["Playing Ps", "Swimming", "dancing"]
-    }
-    return render(request, "home.html", user_data)
+# # def my_home(request):
+# #     return render(request, "base.html")
+# # def blog(request):
+# #     return render(request, "blog.html")
+# # def contact(request):
+# #     return render(request, "contact.html")
+# # def about(request):
+# #     return render(request, "about.html")
+# # def home(request):
+# #     user_data = {
+# #         "name": "Desmond Rono",
+# #         "age": 26,
+# #         "bio": " Engineer!",
+# #         "hobbies": ["Playing Ps", "Swimming", "dancing"]
+# #     }
+# #     return render(request, "home.html", user_data)
