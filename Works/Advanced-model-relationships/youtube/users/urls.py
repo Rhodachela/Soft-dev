fdba8264f49a0efc7e9a0801dabcdd82d1c83055
Rhoda <rhodachela@gmail.com>
2024-11-11 23:10:33 +0300

    Configurings

--- Works/Advanced-model-relationships/youtube/users/urls.py
+++ Works/Advanced-model-relationships/youtube/users/urls.py
@@ -1,14 +1,19 @@
-from django.urls import path
-from . import views
-from .views import my_home, blog, contact, about, home 
-app_name = "users"
+# from django.urls import path
+# from . import views
+# from .views import my_home, blog, contact, about, home
+# from django.contrib.auth.views import LoginView
+# from django.contrib.auth.views import LogoutView
+
+# # app_name = "users"
 
 
-urlpatterns = [
-    # path("R.chela/", view = welcome, name = "R.chela" ),
-    path('home/', views.home, name = "home"),
-    path('about/', views.about, name = "about"),
-    path('contact/', views.contact, name = "contact"),
-    path('blog/', views.blog, name = "blog"),
-    path('', views.my_home, name = "R.chela")
-]
+# # urlpatterns = [
+# #     # path("R.chela/", view = welcome, name = "R.chela" ),
+# #     path('home/', views.home, name = "home"),
+# #     path('about/', views.about, name = "about"),
+# #     path('contact/', views.contact, name = "contact"),
+# #     path('blog/', views.blog, name = "blog"),
+# #     path('', views.my_home, name = "R.chela"),
+# #     path('login/', LoginView.as_view(template_name = "registration/login.html"), name = "login"),
+# #     path('logout/', LogoutView.as_view(), name = "logout"),
+# # ]
