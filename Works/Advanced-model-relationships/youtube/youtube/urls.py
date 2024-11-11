bc5d9243d8fa7afe9b7a62201187c5d15144edc5
Rhoda <rhodachela@gmail.com>
2024-11-11 23:08:01 +0300

    Configuring urls

--- Works/Advanced-model-relationships/youtube/youtube/urls.py
+++ Works/Advanced-model-relationships/youtube/youtube/urls.py
@@ -19,6 +19,11 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("users.urls", namespace="users")),
+    # path('', include("users.urls", namespace="users")),
+    path('accounts/', include("accounts.urls")),
 
+    # path('accounts/profile/', 
+    #     TemplateView.as_view(template_name = "accounts/profile.html", 
+    #     name = "profile")),
+    # path('signup/', SignUpView.as_view(), name = "templates/registration/signup"),
 ]
