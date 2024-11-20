d73084b17268af53c960f288404bc079c2ac5462
Rhoda <rhodachela@gmail.com>
2024-11-20 23:14:55 +0300

    models file

--- Works/wxy-week/Canon/relationship_app/models.py
+++ Works/wxy-week/Canon/relationship_app/models.py
@@ -15,7 +15,7 @@ class Book(models.Model):
         return self.title
 
 class Library(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=200)
     books = models.ManyToManyField(Book, related_name="libraries")
 
     def __str__(self):
