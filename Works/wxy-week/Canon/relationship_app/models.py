f1c3b24773f397fef565b2278e1d88a5cc4d9bc1
Rhoda <rhodachela@gmail.com>
2024-11-22 23:51:41 +0300

    Models file

--- Works/wxy-week/Canon/relationship_app/models.py
+++ Works/wxy-week/Canon/relationship_app/models.py
@@ -22,7 +22,7 @@ class Library(models.Model):
         return self.name
 
 class Librarian(models.Model):
-    name = models.CharField(max_length=300)
+    name = models.CharField(max_length=200)
     library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name="librarians")
 
     def __str__(self):
