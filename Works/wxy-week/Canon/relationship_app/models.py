3a00374947bb31e6af4640fcf5a55e38eb4486fc
Rhoda <rhodachela@gmail.com>
2024-11-22 23:50:56 +0300

    Models file

--- Works/wxy-week/Canon/relationship_app/models.py
+++ Works/wxy-week/Canon/relationship_app/models.py
@@ -8,7 +8,7 @@ class Author(models.Model):
         return self.name
 
 class Book(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=100)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
 
     def __str__(self):
