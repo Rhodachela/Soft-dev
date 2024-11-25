c9911b421d36f3669db029b0222fb348cb01adef
Rhoda <rhodachela@gmail.com>
2024-11-25 12:28:34 +0300

    Removed Canon for a new project

--- Works/wxy-week/Canon/relationship_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import views
-
-urlpatterns = [
-    path('books/', views.list_all_books, name= "list_books"),
-    parh('library/', views.LibraryDetailView.as_view(template_name= "library_detail")),
-]
