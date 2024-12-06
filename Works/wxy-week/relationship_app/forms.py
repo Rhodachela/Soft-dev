9bd707b1037bc6520c57abccebcaf908539f1ffa
Rhoda <rhodachela@gmail.com>
2024-12-07 02:37:28 +0300

    Forms.py file

---
+++ Works/wxy-week/relationship_app/forms.py
@@ -0,0 +1,11 @@
+# bookshelf/forms.py
+
+from django import forms
+
+class BookSearchForm(forms.Form):
+    q = forms.CharField(max_length=100, required=False)
+
+class ExampleForm(forms.Form):
+    name = forms.CharField(max_length=100, required=True, label="Your Name")
+    email = forms.EmailField(required=True, label="Your Email")
+    message = forms.CharField(widget=forms.Textarea, required=True, label="Your Message")
