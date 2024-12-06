3178c698d4b05136860c767628da5d77b518a0b3
Rhoda <rhodachela@gmail.com>
2024-12-07 02:38:30 +0300

    Configuring admin interface

--- Works/wxy-week/relationship_app/admin.py
+++ Works/wxy-week/relationship_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from.models import Book
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
 # Register your models here.
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')
+    list_filter = ('author', 'publication_year')
+    search_fields = ('title', 'author')
