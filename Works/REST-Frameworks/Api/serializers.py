69065a5fbbd5b69a00ff4eb84096ed4c61208ca0
Rhoda <rhodachela@gmail.com>
2024-12-11 23:07:17 +0300

    Ecommerce Project example

--- Works/REST-Frameworks/Api/serializers.py
+++ Works/REST-Frameworks/Api/serializers.py
@@ -22,3 +22,31 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = ["id", "title", "content", "author", "created_at"]
+
+class CommentSerializer(serializers.ModelSerializer):
+    author_name = serializers.CharField(source="author.usename", read_only=True)
+
+    class Meta:
+        model = Post
+        fields = "__all__"
+
+class BlogPostSerializer(serializers.ModelSerializer):
+    Comments = CommentSerializer(many=True, read_only=True)
+    class Meta:
+        model = Post
+        fields = "__all__"
+
+    def validate(self, data):
+        if len(data[title]) < 5:
+            raise serializers.ValidationError("Title must be at least 5 characters long")
+        return data
+
+class UserSerializer(serializers.ModelSerializer):
+    full_name = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Post
+        fields = ["id","email", "username", "first_name", "last_name", "full_name" ]
+
+        def get_full_name(self, obj):
+            return f"{obj.first_name} {obj.last_name}" if obj.first_name and obj.last_name else obj.username
