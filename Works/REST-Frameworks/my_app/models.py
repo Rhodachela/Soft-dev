a6e5132bc1cd4941acf28d26c7ef4eda514bdf32
Rhoda <rhodachela@gmail.com>
2024-12-12 23:55:46 +0300

    Models file

--- Works/REST-Frameworks/my_app/models.py
+++ Works/REST-Frameworks/my_app/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Book(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=255)
     author = models.CharField(max_length=200)
     published_date = models.DateField()
     created_at = models.DateTimeField(auto_now_add=True)
