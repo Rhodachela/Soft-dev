3eab7bc18be2f1849ee67286cd592c6f1a4d2d3d
Rhoda <rhodachela@gmail.com>
2024-12-17 23:50:48 +0300

    Charfield modification

--- Works/REST-Frameworks/my_app/models.py
+++ Works/REST-Frameworks/my_app/models.py
@@ -9,7 +9,7 @@ class Book(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
 class Post(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=100)
     content = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
