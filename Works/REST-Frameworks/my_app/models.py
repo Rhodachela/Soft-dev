ac6c0ecb559fd947174ba7e64ac703cb40adfc85
Rhoda <rhodachela@gmail.com>
2025-01-20 15:42:49 +0300

    Added some new changes to this directory

--- Works/REST-Frameworks/my_app/models.py
+++ Works/REST-Frameworks/my_app/models.py
@@ -12,4 +12,3 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
