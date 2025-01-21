9a9cab70e33e668d0bff8e8e42c25c45a1780f83
Rhoda <rhodachela@gmail.com>
2025-01-21 23:21:40 +0300

    Model of the post

--- Works/DRF/Post/models.py
+++ Works/DRF/Post/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=200)
     content = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateField(auto_now_add=True)
