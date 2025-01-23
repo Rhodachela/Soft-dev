b0f7fd88b9bce322d27d45cb284e2a849440d38d
Rhoda <rhodachela@gmail.com>
2025-01-23 16:30:48 +0300

    Added a searchfield class to the views

--- Works/Intro-to-APIs/api/views.py
+++ Works/Intro-to-APIs/api/views.py
@@ -6,6 +6,7 @@ from rest_framework.generics import ListAPIView
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated, IsAdminUser, IsAuthenticatedOrReadOnly
 from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
 
 # Create your views here.
 
@@ -17,7 +18,8 @@ class BookList(generics.ListAPIView):
 class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    # permission_classes = [IsAuthenticated]
-    # authentication_classes = [TokenAuthentication]
+    search_fields = ['title', 'author']
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [TokenAuthentication]
 
 
