47c9b4135bd837ca2221b641328ed967a4837549
Rhoda <rhodachela@gmail.com>
2025-01-27 19:52:20 +0300

    Models file

--- Works/Advanced-API/api/models.py
+++ Works/Advanced-API/api/models.py
@@ -14,7 +14,7 @@ class Book(models.Model):
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
 
     def __str__(self):
-        return f"{self.title} by {self.author}"
+        return f"{self.title} By {self.author}"
 
     def clean(self):
         # Add custom validation logic
