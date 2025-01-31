c1ebe1f0748c365a4f1dfb659357bd0adedc7984
Rhoda <rhodachela@gmail.com>
2025-01-31 21:35:20 +0300

    Book creation view

--- Works/Advanced-API/api/urls.py
+++ Works/Advanced-API/api/urls.py
@@ -8,9 +8,9 @@ from .views import (
 )
 
 urlpatterns = [
-    path('books/create/', BookCreateView.as_view(), name='book-create'),
+    path('books/create/', BookCreateView.as_view(), name='book-create'), # Book creation view
     path('books/', BookListView.as_view(), name = 'book-list'),
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
     path('books/update/', BookUpdateView.as_view(), name='book-update'),
-    path('books/delete/', BookDeleteView.as_view(), name='book-delete'),
+    path('books/delete/', BookDeleteView.as_view(), name='book-delete'), 
 ]
